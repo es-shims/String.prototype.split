ef5b3cd7ad095fb470734ec8196a0324fd39edb2
Jordan Harband <ljharb@gmail.com>
2020-11-19 22:03:44 -0800

    [Robustness] cache `String.prototype.split.apply` in the implementation

--- implementation.js
+++ implementation.js
@@ -1,8 +1,11 @@
 'use strict';
 
 // var ToObject = require('es-abstract/2020/ToObject');
+var callBind = require('call-bind');
+
+var $split = callBind.apply(String.prototype.split);
 
 module.exports = function split() {
 	// var O = ToObject(this);
-	return String.prototype.split.apply(this, arguments);
+	return $split(this, arguments);
 };
